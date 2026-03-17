fd2daecc4a57b178a80e328cc78c7e9964b23d7f
MuhammadSyed2 <123975019+MuhammadSyed2@users.noreply.github.com>
2026-03-17 15:48:54 +0600

    stripe payment create alternate method

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -18,6 +18,16 @@ class StripeController extends Controller
     public function store(Request $request)
     {
         // dd($request->all());
+
+        // $session = $stripe->checkout->sessions->create([
+        //     'payment_method_types' => ['card'],
+        //     'line_items'           => $lineItems,
+        //     'mode'                 => 'payment',
+        //     'success_url'          => config('app.frontend_url') . '/order/success?order_id=' . $order->id,
+        //     'cancel_url'           => config('app.frontend_url') . '/order/cancel?order_id=' . $order->id,
+        //     'metadata'             => ['order_id' => $order->id],
+        // ]);
+
         $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET'));
         $charge = $stripe->charges->create([
             'amount' => $request->price * 100,
